a70975cd6b4c42f85c8c6da652c7859cfb3b3807
Thomas Haddad <haddad.thomas19@gmail.com>
2024-11-27 19:46:24 +0000

    fixed

--- app.py
+++ app.py
@@ -247,6 +247,9 @@ def encode_url():
 def decode_url():
     text = request.form.get('text')
     return jsonify({'result': unquote(text)})
+@app.route('/')
+def index:
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
